33515986991a0a58577b1a60a3bc5a3da1c21b9e
veronikaDyshnyk <veronika199942@gmail.com>
2023-01-11 13:25:33 +0000

    відловлює запит на кожну букву а не сло цілком, чому?:(

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Weather from "./components/Weather";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const [data, setData] = useState([]);
+    const [cityName, setCityName] = useState("");
+// const cityName = "lviv";
+
+    // useEffect(() => {
+    //     const fetchData  = () => {
+    //         // navigator.geolocation.getCurrentPosition(function(position) {
+    //         //     setLat(position.coords.latitude);
+    //         //     setLong(position.coords.longitude);
+    //         // });
+    //         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4631b8e3f2231a4b4679e2aa1c4aadb1`)
+    //             .then(res => res.json())
+    //             .then(data => {
+    //                 setData(data)
+    //                 console.log(data);
+    //             });
+    //     }
+    //     fetchData();
+    // }, [])
+
+    const fetchData  = (event) => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4631b8e3f2231a4b4679e2aa1c4aadb1`)
+            .then(res => res.json())
+            .then(data => {
+                setData(data)
+                console.log(data);
+            });
+    }
+
+    return (
+            // <div className="app">
+            //     {(typeof data.main != 'undefined') ? (
+            //         <Weather weatherData={data}/>
+            //     ) : (
+            //         <div></div>
+            //     )}
+            //
+            // </div>
+
+        <div>
+            <input
+                typeof="text"
+                className="input"
+                placeholder="enter city"
+                onChange={event => setCityName(event.target.value)}
+                value={cityName}
+
+                onKeyDown={fetchData}
+            />
+        </div>
+
+        );
+    }
+
+    export default App;
